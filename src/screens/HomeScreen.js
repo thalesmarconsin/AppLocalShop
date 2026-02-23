15463179675c34e027ce7f8bcf49ab758b715a2f
Thales <157228561+thalesmarconsin@users.noreply.github.com>
2026-02-23 10:09:04 -0300

    Codigo atualizado ada home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,4 +13,23 @@ export default function HomeScreen() {
                 contentContainerStyle={styles.list}
                 />
         </View>
-    ); }
+    ); 
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        backgroundColor: '#f5f5f5',
+        paddingTop: 50,
+    },
+    headerTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginLeft: 20,
+        marginBottom: 20,
+        color: '#333',
+    },
+    list: {
+        paddingHorizontal: 20,
+    }
+});
---
+++ src/screens/StyleHome.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        backgroundColor: '#f5f5f5',
+        paddingTop: 50,
+    },
+    headerTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginLeft: 20,
+        marginBottom: 20,
+        color: '#333',
+    },
+    list: {
+        paddingHorizontal: 20,
+    }
+});
